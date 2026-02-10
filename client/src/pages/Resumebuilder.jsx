a4709587f84990af146f9cfd1baf442c59300654
pavani285cse <pavanipriya285@gmail.com>
2026-02-10 20:51:30 +0530

    2nd commit

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React, { useState,useEffect } from 'react'
+import { dummyResumeData } from '../assets/assets'
+import { ArrowLeftIcon } from 'lucide-react'
+import { Link } from 'react-router-dom'
 const Resumebuilder = () => {
+  const [resumeData,setResumeData]=useState({
+    _id: '',
+    title:'',
+    personal_info:'',
+    experience:[],
+    education:[],
+    skills:[],
+    projects:[],
+    template: "classic",
+    public: false,
+  })
+  const loadExistingResume = async(resumeId) => {
+    const resume=dummyResumeData.find(resume=> resume._id === resumeId)
+    if(resume){
+      setResumeData(resume)
+      document.title=resume.title
+    }
+  }
+  useEffect(()=>{
+    loadExistingResume()
+  },[])
   return (
     <div>
-      <h1>Resume Builder</h1>
+      <div>
+        <Link to={'/app'} className='inline-flex gap-2 items-center text text-slate-500 hover:text-slate-700 transition-all'>
+          <ArrowLeftIcon className='size-4'/>Back to Dashboard
+        </Link>
+      </div>
     </div>
   )
 }
