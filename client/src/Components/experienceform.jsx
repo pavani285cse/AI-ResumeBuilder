bee9b068763ffa730a5942f2c357b9ebe7154025
pavani285cse <pavanipriya285@gmail.com>
2026-04-24 13:38:42 +0530

    next commit

--- client/src/Components/experienceform.jsx
+++ client/src/Components/experienceform.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Briefcase } from 'lucide-react'
 const Experienceform = ({data, onChange,setResumeData}) => {
     const addExperience = () => {
         const newExperience={
@@ -28,11 +28,34 @@ const Experienceform = ({data, onChange,setResumeData}) => {
                 <h3 classNmae='flex items-center gap-2 text-lg font-semibold text-gray-900'>Professional Experience</h3>
                 <p className='text-sm text-gray-500'>Add your work experience for you resume here</p>
             </div>
-            <button className='flexitems-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50'>
+            <button onClick={addExperience} className='flexitems-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50'>
               <Plus className='size-4'/>
               Add Experience  
             </button>       
         </div>
+        { data.length===0 ? (
+                <div className='text-center py-8 text-gray-500'>
+                    <Briefcase className="w-12 h-12 mx-auto mb-3 text-gray-300" />
+                    <p>No experience added yet</p>
+                    <p className='text-sm'>Click "Add Experience" to get started</p>
+                </div>
+            ):(
+        <div className='space-y-4'>
+            {data.map((experience,index)=>(
+                <div key={index} className='p-4 border border-gray-200 rounded-lg space-y-3'>
+                    <div className='flex items-start justify-between'>
+                        <h4 className='text-lg font-semibold text-gray-900'>{experience.position}</h4>
+                        <button onClick={() => removeExperience(index)}
+  className="text-red-500 hover:text-red-700 transition-colors"
+>
+  <Trash2 className="size-4" />
+</button>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+      )}
     </div>
   )
 }
